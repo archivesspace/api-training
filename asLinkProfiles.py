219dc23066a82bfee2bba516176bc971475ff077
Mark Custer <fordmadox@gmail.com>
2018-06-24 16:43:28 -0400

    updated everything to python except for the viaf scrips so far

--- asLinkProfiles.py
+++ asLinkProfiles.py
@@ -1,11 +1,12 @@
-import json, requests, secrets, time
+import json, requests, authenticate, runtime
+# since we're going to re-use the baseURL and headers variables, we'll import them here
+from authenticate import baseURL, headers
 
-startTime = time.time()
-
-# function to find key in nested dicts: see http://stackoverflow.com/questions/9807634/find-all-occurences-of-a-key-in-nested-python-dictionaries-and-lists
+# function to find key in nested dictionaries: see http://stackoverflow.com/questions/9807634/find-all-occurences-of-a-key-in-nested-python-dictionaries-and-lists
+# and now we're getting fancy!
 def gen_dict_extract(key, var):
-    if hasattr(var,'iteritems'):
-        for k, v in var.iteritems():
+    if hasattr(var,'items'):
+        for k, v in var.items():
             if k == key:
                 yield v
             if isinstance(v, dict):
@@ -16,41 +17,18 @@ def gen_dict_extract(key, var):
                     for result in gen_dict_extract(key, d):
                         yield result
 
-# import secrets
-baseURL = secrets.baseURL
-user = secrets.user
-password = secrets.password
-
-# authenticate
-auth = requests.post(baseURL + '/users/'+user+'/login?password='+password).json()
-session = auth["session"]
-headers = {'X-ArchivesSpace-Session':session}
-
-# test for successful connection
-def test_connection():
-	try:
-		requests.get(baseURL)
-		print 'Connected!'
-		return True
-
-	except requests.exceptions.ConnectionError:
-		print 'Connection error. Please confirm ArchivesSpace is running.  Trying again in 10 seconds.'
-
-is_connected = test_connection()
-
-while not is_connected:
-	time.sleep(10)
-	is_connected = test_connection()
+# This is where we connect to ArchivesSpace.  See authenticate.py
+authenticate.test_connection()
 
 # provide instructions
-print 'This script is used to link all top_containers in a single collection (identified by the ArchivesSpace resource id number) to a single container_profile (identified by the ArchivesSpace container_profile id number).'
-raw_input('Press Enter to continue...')
+print ('This script is used to link all top_containers in a single collection (identified by the ArchivesSpace resource ID number) to a single container_profile (identified by the ArchivesSpace container_profile ID number).')
+input('Press Enter to continue...')
 
 # have user enter resource id
-resource_id = raw_input('Enter resource id: ')
+resource_id = input('Enter resource ID (in this case, you should enter 1): ')
 
 # search for top_containers linked to entered resource id
-endpoint = '/repositories/2/top_containers/search?page=1&aq={"filter_term":{"field":"collection_uri_u_sstr", "value":"/repositories/2/resources/' + resource_id + '", "jsonmodel_type":"field_query"}}'
+endpoint = '/repositories/2/top_containers/search?page=1&aq={"filter_term":{"field":"collection_uri_u_sstr", "value":"/repositories/2/resources/ + resource_id", "jsonmodel_type":"field_query"}}'
 output = requests.get(baseURL + endpoint, headers=headers).json()
 
 # populate top_containers with the ids of each top_container in search results
@@ -65,19 +43,13 @@ for top_container in top_containers:
     records.append(output)
 
 # have user enter container profile id
-profile_id = raw_input('Enter container profile id: ')
+profile_id = input('Enter container profile ID (I am going to enter 9. You can select another value, as long that ID is in your instance of ArchivesSpace.): ')
 
 # Add container profile to records and post
-print 'The following records have been updated in ArchivesSpace:'
+print ('The following records have been updated in ArchivesSpace:')
 for record in records:
-    record['container_profile'] = {'ref': '/container_profiles/' + profile_id + ''}
+    record['container_profile'] = {'ref': '/container_profiles/' + profile_id}
     jsonLine = json.dumps(record)
     uri = record['uri']
     post = requests.post(baseURL + uri, headers=headers, data=jsonLine).json()
-    print post
-
-# show script runtime
-elapsedTime = time.time() - startTime
-m, s = divmod(elapsedTime, 60)
-h, m = divmod(m, 60)
-print 'Post complete. Total script run time: ', '%d:%02d:%02d' % (h, m, s)
+    print(post)
--- postBarcodes.py
+++ postBarcodes.py
@@ -1,51 +1,22 @@
-import json, requests, secrets, time, csv, os
-
-startTime = time.time()
-
-# import secrets
-baseURL = secrets.baseURL
-user = secrets.user
-password = secrets.password
-
-#authenticate
-auth = requests.post(baseURL + '/users/'+user+'/login?password='+password).json()
-session = auth["session"]
-headers = {'X-ArchivesSpace-Session':session, 'Content_Type':'application/json'}
-
-# test for successful connection
-def test_connection():
-	try:
-		requests.get(baseURL)
-		print 'Connected!'
-		return True
-
-	except requests.exceptions.ConnectionError:
-		print 'Connection error. Please confirm ArchivesSpace is running.  Trying again in 10 seconds.'
-
-is_connected = test_connection()
-
-while not is_connected:
-	time.sleep(10)
-	is_connected = test_connection()
+import json, requests, csv, authenticate, runtime
+# since we're going to re-use the baseURL and headers variables, we'll import them here
+from authenticate import baseURL, headers
 
 # print instructions
-print 'This script replaces existing fauxcodes with real barcodes (linked in a separate csv file) in ArchivesSpace.'
-raw_input('Press Enter to continue...')
+print ('This script replaces existing fauxcodes with real barcodes (linked in a separate csv file) in ArchivesSpace.')
+input('Press Enter to connect to ArchivesSpace and post those barcodes...')
+
+# This is where we connect to ArchivesSpace.  See authenticate.py
+authenticate.test_connection()
 
 # open csv and generate dict
 reader = csv.DictReader(open('barcodes.csv'))
 
 # GET each top_container listed in top_containers and add to records
-print 'The following barcodes have been updated in ArchivesSpace:'
+print ('The following barcodes have been updated in ArchivesSpace:')
 for row in reader:
 	uri = row['uri']
 	output = requests.get(baseURL + uri, headers=headers).json()
 	output['barcode'] = row['real']
 	post = requests.post(baseURL + uri, headers=headers, data=json.dumps(output)).json()
-	print post
-
-# show script runtime
-elapsedTime = time.time() - startTime
-m, s = divmod(elapsedTime, 60)
-h, m = divmod(m, 60)
-print 'Post complete.  Total script run time: ', '%d:%02d:%02d' % (h, m, s)
+	print (post)
